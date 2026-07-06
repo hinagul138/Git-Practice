f0b2eb4b461099e0d723755f9795a282b7b02b22
Hina <hinagul4679@gmail.com>
2026-07-06 10:45:42 +0500

    Added variables.py practice

---
+++ variables.py
@@ -0,0 +1,18 @@
+name = "Hina"
+print(name)
+age = 20
+print(age)
+country = "pakistan"
+print (country)
+University = "University of Sawabi"
+print(University)
+city = "Swabi"
+print(city)
+Department = "Computer Science"
+print(Department)
+print(name)
+print(age)
+print(country)
+print(University)
+print(city)
+print("My name is", name,"and I am",age,"years old, I live in",country)
