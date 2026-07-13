f00eb9501882bfd791160c2d7ef5dbd2e75c604a
hinagul138 <hinagul4679@gmail.com>
2026-07-12 17:34:07 -0700

    Completed Python print statement

---
+++ print_statment.py
@@ -0,0 +1,73 @@
+print ("task1")
+print("simple message")
+print("Hello,World!")
+
+print("task2")
+print("Multiple Lines")
+print("Python")
+print("is")
+print("fun")
+
+print("task3")
+print("Numbers")
+print(10)
+print(20)
+print(30)
+
+print("task4")
+print("Special Characters")
+print('He said."Python is  awesome!"')
+
+print("task5")
+print("Using Escape Sequence - single print()")
+print("Name:Hina\nAge 20\nCity: Swabi")
+
+print("task6")
+print("Pattern")
+print("*")
+print("***")
+print("*****")
+print("*******")
+
+print("task7")
+print("Box")
+print("*****")
+print("* *")
+print("* *")
+print("*****")
+
+print("task8")
+print("Right-Aligned Triangle")
+print("    *")
+print("   **")
+print("  ***")
+print(" ****")
+print("*****")
+
+print("task9")
+print("Table Format")
+print("Name Marks")
+print("Hina 85")
+print("Aliza 92")
+print("Asma 70")
+
+print("task10")
+print("Advanced Print Challenge")
+print("==========")
+print(" STUDENT ID")
+print("==========")
+print("Name Hina")
+print("Smester 7")
+print("Roll 198")
+
+print("task11")
+print("  /\\")
+print(" /  \\")
+print("/____\\")
+print("|    |")
+print("|____|")
+
+
+
+
+
