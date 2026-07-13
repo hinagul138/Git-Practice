7b1bf434a2e8da544bbee0f75b3883faa8a3b476
hinagul138 <hinagul4679@gmail.com>
2026-07-13 10:32:45 -0700

    Adding Variabled Practice

---
+++ Variables_Practice.py
@@ -0,0 +1,87 @@
+print("Hina")
+print("20")
+age = 20
+name = "Hina"
+print(age)
+print(name)
+
+
+x = 90
+print(x)
+print(type(x))
+
+a = 5
+b = 10
+c = 15
+print(a+b+c)
+
+print("Hina")
+print('Hina')
+
+x = 60
+print(x)
+x = str (x)
+print(x)
+
+x = 2
+y = str(x)
+print(y)
+print(type(y))
+
+x = "3"
+y = int(x)
+print(y)
+print(type(y))
+  
+x = 4
+y = float(x)
+print(y)
+print(type(y))
+
+x = 2
+print("the type of x is",type(x))
+x=str(x)
+print("the type of x is",type(x))
+x=int(x)
+print("the type of x is",type(x))
+x=float(x)
+print("the type of x is",type(x))
+
+x = 1
+y = 2
+sum=x+y
+produt=x*y
+print("the SUM of x and y is:",x+y )
+print("the produt of x and  y  is:",x*y)
+
+x = 1
+y = 2
+print("the product of x and y is:",x*y)
+x = 1
+y = 2
+z = x*y
+print("the produt of x and y is:", z)
+ 
+x = 1
+y = 2
+print("the sum of x and y is:",x+y)
+x = 1
+y = 2
+z= x+y
+print("the sum of x and y is:",z)
+
+
+x = 40
+X = 40
+print(X)
+print(X)
+
+x = 50
+X = 40
+print(x)
+print(X)
+
+X = 50
+x = 40
+print(X)
+print(x)
