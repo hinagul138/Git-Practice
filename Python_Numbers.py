6f0f51000e364ff5f3f1aa87bc447e02aab23044
hinagul138 <hinagul4679@gmail.com>
2026-07-16 17:43:54 -0700

    Add Python Numbers Assignment

---
+++ Python_Numbers.py
@@ -0,0 +1,59 @@
+int=10
+float=10.5
+complex=2+3j
+
+a = 10
+b = 10.5
+c = 2+3j
+print(a)
+print(b)
+print(c)
+
+a = 15
+b = 15.5
+c = 22j
+print(type(a))
+print(type(b))
+print(type(c))
+
+15
+15.5
+2j
+print(type(15))
+print(type(15.1))
+print(type(2j))
+
+a = (20)
+b = (8.9)
+c = (5)
+print(float(a))
+print(int(b))
+print(complex(c))
+
+a = 1.2e3
+b= 5.6e-2
+print(a) 
+print(b)
+print(type(a))
+print(type(b))
+
+x = 10
+print(type(x))
+
+x = 10.5
+print(type(x))
+
+x = 2+3j
+print(type(x))
+
+import random
+print(random.randint(1,10))
+
+a = 7.8
+b = int(a)
+print((b))
+
+
+
+a = 5+2j
+print(int(a))
